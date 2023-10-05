1d0fef2631cd6205b6388b5e5ed41c76801ff52a
Maksym Marchuk <maxxik007@gmail.com>
2023-10-06 01:10:32 +0200

    [MOD]: Deleted unused import

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -11,7 +11,6 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
